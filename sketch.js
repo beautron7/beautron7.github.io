d9eaf90aba15a270a85e28314bf47865b6598e6e
beautron7 <beauvandenburgh19@newmanschool.org>
2016-09-30 10:15:29 -0500

    scale

--- sketch.js
+++ sketch.js
@@ -622,7 +622,8 @@ function preload(){
 function setup() {//p5
 	createCanvas(950, 600);
 	Grid.initialize();
-	buy_menu = createGraphics(150*3,height*3);
+	var buy_scale = prompt("Please enter the buy menu scale (if unsure, type 1)");
+	buy_menu = createGraphics(150*buy_scale,height*buy_scale);
 	frameRate(15);
 	for(var o = 0; o < 4; o++){
 			Grid.addSpawn(0, o);
