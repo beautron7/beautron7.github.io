2556e7b602782cbecef0ba1323455daebc742c1f
beautron7 <beauvandenburgh19@newmanschool.org>
2017-02-09 15:40:11 -0600

    added specific heat stuff

--- SpecificHeat.py
+++ SpecificHeat.py
@@ -4,16 +4,18 @@ print("0: Specific heat of object (joules / (grams*Celcius))")
 print("1: Temp Change of object")
 print("2: Mass of object")
 print("3: total energy used")
-selection = int(input("What option do you want"));
-parameters = list()
+selection = int(input("What option do you want? "));
+parameters = [0,0,0,0]
+
 if selection is not 0:
-    parameters[0]=input("What is the Specific heat of object (j / (g*C))")
+    parameters[0]=float(input("What is the Specific heat of object (j / (g*C))? "))
 if selection is not 1:
-    parameters[1]=input("What is the Temp Change of object")
+    parameters[1]=float(input("What is the Temp Change of object? "))
 if selection is not 2:
-    parameters[2]=input("What is the Mass of object")
+    parameters[2]=float(input("What is the Mass of object? "))
 if selection is not 3:
-    parameters[3]=input("What is the total energy used")
+    parameters[3]=float(input("What is the total energy used? "))
+
 if selection is 0:
     answer = str(parameters[3]/(parameters[2]*parameters[1]))
     print("The Specific heat of object is "+answer+" (j / (g*C))")
